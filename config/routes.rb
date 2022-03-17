60aab718a48034f0939596c25aad42b5534a28ef
malexander15 <creepinjezus15@gmail.com>
2022-03-17 12:40:37 -0400

    updated home page, added an about me and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  get 'simple_pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "simple_pages#index"
 end
